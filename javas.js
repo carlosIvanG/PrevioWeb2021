956a0876fd92b0b0c3ee59a0bf90a1c22d6bebbc
carlosIvanG <carlosivangc@ufps.edu.co>
2021-04-12 10:18:27 -0500

    agregando modificaciones a la pagina.version 2

--- javas.js
+++
@@ -1,6 +0,0 @@
-/* function escribir(){
-    let fecha=new Date()
-    document.getElementById('cont1').innerHTML=fecha.toDateString;
-    } */
-txt = document.getElementById("txt");
-document.getElementById('cont1').innerHTML=fecha.toDateString;
