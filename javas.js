a37d4642845966b9d0aad8dfb262076687ac5f7e
carlosIvanG <carlosivangc@ufps.edu.co>
2021-04-12 09:21:14 -0500

    agregando contenido a la paagina

---
+++ javas.js
@@ -0,0 +1,6 @@
+/* function escribir(){
+    let fecha=new Date()
+    document.getElementById('cont1').innerHTML=fecha.toDateString;
+    } */
+txt = document.getElementById("txt");
+document.getElementById('cont1').innerHTML=fecha.toDateString;
